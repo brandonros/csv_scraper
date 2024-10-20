b256305c050f904c4add609c8751c8df405af578
Brandon Ros <brandonros1@gmail.com>
2024-10-19 20:41:34 -0400

    checkin

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+use std::future::Future;
+use std::pin::Pin;
+use std::sync::Arc;
+use std::time::Duration;
+
+use async_executor::Executor;
+use async_io::Timer;
+use simple_error::SimpleResult;
+
+pub trait ScrapeOperation: Send + Sync {
+    fn execute(&self) -> Pin<Box<dyn Future<Output = SimpleResult<()>> + Send + 'static>>;
+}
+
+pub struct Scraper;
+
+impl Scraper {
+    pub async fn scrape<T: ScrapeOperation + 'static>(
+        executor: Arc<Executor<'static>>,
+        delay: Duration,
+        operation: T,
+    ) -> SimpleResult<()> {
+        loop {
+            let op = operation.execute();
+            let _handle = executor.spawn(async move {
+                match op.await {
+                    Ok(_) => (),
+                    Err(err) => log::error!("error scraping: {err}")
+                }
+            });
+
+            Timer::after(delay).await;
+        }
+    }
+}
